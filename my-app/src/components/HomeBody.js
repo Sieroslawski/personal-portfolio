415793c02ab4610d01e4e02637ce315e17b13ead
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-03-07 16:04:49 -0800

    Home skeleton done

--- my-app/src/components/HomeBody.js
+++ my-app/src/components/HomeBody.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function HomeBody() {
   return (
-    <div>Hello World</div>
+      <section>
+         <div className="content-wrapper">
+             <h1>Adrian Sieroslawski</h1>
+             <p>Full-Stack Web Developer.</p>         
+         <div className="">
+         <a href="https://github.com/Sieroslawski">Github</a>
+         <a href="https://www.linkedin.com/in/adrian-sieroslawski-7aa411228/">LinkedIn</a>
+         </div>         
+         </div>
+      </section>
+   
   )
 }
 
