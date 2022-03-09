c725a0852760eb9a2260cedb96c904fe1e232fd1
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-03-09 13:22:53 -0800

    font awesome / babel installation done, buttons completed

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Footer from '../components/Footer';
 import ParticleBackground from './Particles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 function Home() {
   return (
@@ -11,8 +13,8 @@ function Home() {
              <p><font color="#5cb85c">Full-Stack</font> Web Developer<font color="#5cb85c">.</font></p>
          </div>                      
          <div className="button-links">
-         <a href="https://github.com/Sieroslawski">Github</a>
-         <a href="https://www.linkedin.com/in/adrian-sieroslawski-7aa411228/">LinkedIn</a>       
+         <a href="https://github.com/Sieroslawski" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={brands('github-alt')} /> Github</a>
+         <a href="https://www.linkedin.com/in/adrian-sieroslawski-7aa411228/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={brands('linkedin')} /> LinkedIn</a>       
          </div>        
          <div className="footer">                 
         </div>        
