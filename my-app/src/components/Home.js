99a3506138d121e4aacc545779e4351f6f7d421c
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-03-08 19:44:01 -0800

    home page text adjustments
    margin, padding, font-size adjustments

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -5,18 +5,19 @@ import ParticleBackground from './Particles';
 function Home() {
   return (
       <section>
-         <div className="content-wrapper">           
-             <h1>Adrian Sieroslawski</h1>
-             <p>Full-Stack Web Developer.</p>         
+         <div className="content-wrapper">
+         <div className="home-text">           
+             <h1>Adrian <font color="#5cb85c">Sieroslawski</font></h1>
+             <p><font color="#5cb85c">Full-Stack</font> Web Developer<font color="#5cb85c">.</font></p>
+         </div>                      
          <div className="button-links">
          <a href="https://github.com/Sieroslawski">Github</a>
          <a href="https://www.linkedin.com/in/adrian-sieroslawski-7aa411228/">LinkedIn</a>       
          </div>        
-         <div className="footer">         
-        <Footer/>
-        </div>
-        
+         <div className="footer">                 
+        </div>        
          </div>
+         <Footer/>
          <ParticleBackground className="particles"/>
       </section>
     
