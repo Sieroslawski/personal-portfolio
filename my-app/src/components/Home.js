c5496909e6b326bec70e153e145d0ba2e5878fff
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-03-08 19:09:02 -0800

    enabled and configured particles-ts library in home page

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import Footer from '../components/Footer';
+import ParticleBackground from './Particles';
 
 function Home() {
   return (
       <section>
-         <div className="content-wrapper">
+         <div className="content-wrapper">           
              <h1>Adrian Sieroslawski</h1>
              <p>Full-Stack Web Developer.</p>         
          <div className="button-links">
          <a href="https://github.com/Sieroslawski">Github</a>
          <a href="https://www.linkedin.com/in/adrian-sieroslawski-7aa411228/">LinkedIn</a>       
-         </div>
-         <div className="footer">
-        <Footer/>
-        </div> 
          </div>        
+         <div className="footer">         
+        <Footer/>
+        </div>
+        
+         </div>
+         <ParticleBackground className="particles"/>
       </section>
     
   )
