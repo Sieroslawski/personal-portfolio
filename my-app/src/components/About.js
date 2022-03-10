9c7d0d7d6a4d0b0e4daebb4287e20937b4883037
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-03-09 18:49:26 -0800

    css work - making about page responsive

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -4,17 +4,15 @@ import image from '../images/Adrian Sieroslawski (1).jpg'
 function About() {
   return (
     <section>
-        <div className="about-wrapper">        
-            <h1 className="title-description">About Me</h1>        
+        <div className="about-wrapper">                     
         <div className="description">
+        <h1 className="title-description">About Me</h1>       
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
         It has survived not only five centuries, but also the leap into electronic typesetting, 
         remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
         and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </div>
-        <img src={image}></img>
-        <div className="skill-container">
+        <div className="skill-container">         
             <h2 className="title-description">Skills</h2>
             <p>HTML5</p>
             <p>CSS</p>
@@ -27,7 +25,11 @@ function About() {
             <p>ASP.NET</p>
             <p>SQL</p>
             <p>AWS</p>           
-        </div>       
+        </div>
+        </div>
+        <div className="headshot">
+            <img src={image}></img> 
+        </div>                      
     </div>
     </section>
   )
