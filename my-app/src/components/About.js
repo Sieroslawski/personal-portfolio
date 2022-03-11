800ce2af0742a86ac1dddbb1d3ea4ba9a19af316
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-03-10 16:18:07 -0800

    added media queries, styling, font change, animations,

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -2,18 +2,19 @@ import React from 'react'
 import image from '../images/Adrian Sieroslawski (1).jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { brands, solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+import 'animate.css';
 
 function About() {
   return (
-    <section>
-        <div className="about-wrapper">                     
+    <section className="about" >
+        <div className="about-wrapper animate__animated animate__fadeIn">                     
         <div className="description">
-        <h1 className="title-description">About Me</h1>       
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        <h1 className="title-description">About Me</h1>               
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
         It has survived not only five centuries, but also the leap into electronic typesetting, 
         remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         <div className="skill-container">         
             <h2 className="title-description">Skills</h2>
             <p>HTML5 <FontAwesomeIcon icon={brands('html5')}  size="3x" className="html5-icon"/></p>
@@ -55,7 +56,7 @@ function About() {
         </div>
         </div>
         <div className="headshot">
-            <img src={image}></img> 
+            <img src={image} alt="portfolio"></img> 
         </div>                      
     </div>
     </section>
