d0688041f90f7f7be12d6a237253b187105131f3
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-03-27 14:58:12 -0700

    install MDB bootsrap and create basic Card component for projects page

--- my-app/src/pages/PageProjects.js
+++ my-app/src/pages/PageProjects.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer';
+import Projects from '../components/Projects';
 
 function PageProjects() {
   return (
     <div>        
     <Nav/>
+    <Projects/>
     <Footer/>
     </div>
   )
